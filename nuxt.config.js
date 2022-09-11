1e3bbf9300ea62df2b22c83829629ece9a718c45
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-09-12 02:51:34 +0900

    add reset.css.

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/style/reset.css', 'modern-css-reset'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
