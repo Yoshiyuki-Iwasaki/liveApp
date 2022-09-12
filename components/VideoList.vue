69278c7b1f31ac893bb9ac0802a9dcb7c45cd052
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-09-13 05:14:02 +0900

    fix videoList component layout.

--- components/VideoList.vue
+++ components/VideoList.vue
@@ -1,28 +1,34 @@
 
 <template>
   <ul class="list">
-    <li class="list_item">
+    <li class="list-item">
       <a class="link" href="/video/1">
         <figure>
-          <img src="" alt="">
+          <img src="@/assets/img/thumb.png" alt="">
         </figure>
-        <h2 class="title">Video名が入ります。Video名が入ります。Video名が入ります。Video名が入ります。</h2>
+        <div class="txt-area">
+          <h2 class="title">Video名が入ります。Video名が入ります。Video名が入ります。Video名が入ります。</h2>
+        </div>
       </a>
     </li>
-    <li>
+    <li class="list-item">
       <a class="link" href="/video/2">
         <figure>
-          <img src="" alt="">
+          <img src="@/assets/img/thumb.png" alt="">
         </figure>
-        <h2 class="title">Video名が入ります。Video名が入ります。Video名が入ります。Video名が入ります。</h2>
+        <div class="txt-area">
+          <h2 class="title">Video名が入ります。Video名が入ります。Video名が入ります。Video名が入ります。</h2>
+        </div>
       </a>
     </li>
-    <li>
+    <li class="list-item">
       <a class="link" href="/video/3">
         <figure>
-          <img src="" alt="">
+          <img src="@/assets/img/thumb.png" alt="">
         </figure>
-        <h2 class="title">Video名が入ります。Video名が入ります。Video名が入ります。Video名が入ります。</h2>
+        <div class="txt-area">
+          <h2 class="title">Video名が入ります。Video名が入ります。Video名が入ります。Video名が入ります。</h2>
+        </div>
       </a>
     </li>
   </ul>
@@ -31,6 +37,17 @@
 <style scoped>
 .list {
   display: flex;
+  flex-wrap: wrap;
+  column-gap: 5rem;
+}
+
+.list-item {
+  flex-basis: 300px;
+  border: .1rem solid #333;
+}
+
+.txt-area {
+  margin: 1.5rem 1rem;
 }
 
 .title {
