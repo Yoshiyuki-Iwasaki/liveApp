67aaab6a453223d95a27a355632046b5ec03561c
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-09-13 05:15:06 +0900

    remove unnecessary file.

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,18 +4,15 @@
     <div class="content">
       <Nuxt />
     </div>
-    <FooterVue/>
   </div>
 </template>
 <script>
 import HeaderVue from '../components/Header.vue'
-import FooterVue from '../components/Footer.vue'
 
 export default {
   name: 'DefaultLayout',
   components: {
     HeaderVue,
-    FooterVue
   },
 }
 </script>
