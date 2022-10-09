e11a692eab60e519f88f493624997cf4c18ffee5
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-10-10 01:25:35 +0900

    create signUp page.

--- src/pages/signUp/index.vue
+++ src/pages/signUp/index.vue
@@ -1,8 +1,22 @@
 <template>
-  <p>SignUp</p>
+  <section>
+    <h2>ユーザ登録画面</h2>
+    <form @submit.prevent="registerUser">
+      <div class="form-group">
+        <label for="email">メールアドレス:</label>
+        <input id="email" v-model="email" type="email" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="password">パスワード:</label>
+        <input id="password" v-model="password" type="password" class="form-control">
+      </div>
+      <button type="submit" class="btn btn-info">登録する</button>
+    </form>
+  </section>
 </template>
 
 <script lang="js">
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export default {
   name: 'SignUpPage',
@@ -12,9 +26,17 @@ export default {
     return {
     }
   },
-  computed: {
-  },
   methods: {
-  }
+    registerUser() {
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, this.email, this.password)
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log('errorCode', errorCode);
+          console.log('errorMessage', errorMessage);
+        });
+    },
+  },
 }
 </script>
