ff646e68fde4c5943da6acba9bc51d594d601dc7
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-10-10 14:54:49 +0900

    fix signIn and signUp page layout.

--- src/pages/signIn/index.vue
+++ src/pages/signIn/index.vue
@@ -1,23 +1,24 @@
 <template>
-  <section>
-    <h2>ログイン画面</h2>
-    <form @submit.prevent="loginUser">
-      <div class="form-group">
-        <label for="email">メールアドレス:</label>
-        <input id="email" v-model="email" type="email" class="form-control">
+  <div class="inner">
+    <h1 class="ttl">ログイン画面</h1>
+    <form class="form" @submit.prevent="loginUser">
+      <div class="form-list">
+        <label class="form-label" for="email">メールアドレス:</label>
+        <input id="email" v-model="email" class="form-input" type="email">
       </div>
-      <div class="form-group">
-        <label for="password">パスワード:</label>
-        <input id="password" v-model="password" type="password" class="form-control">
+      <div class="form-list">
+        <label class="form-label" for="password">パスワード:</label>
+        <input id="password" v-model="password" class="form-input" type="password">
       </div>
-      <button type="submit" class="btn btn-info">ログインする</button>
+      <div class="btn-wrap">
+        <button type="submit" class="btn">ログインする</button>
+      </div>
     </form>
-  </section>
+  </div>
 </template>
 
 <script lang="js">
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
 
 export default {
   name: 'SignInPage',
@@ -30,7 +31,11 @@ export default {
   methods: {
     loginUser() {
       const auth = getAuth();
-      signInWithEmailAndPassword(auth, this.email, this.password).catch(function (error) {
+      signInWithEmailAndPassword(auth, this.email, this.password)
+        .then(function () {
+          this.$router.push('/');
+        })
+        .catch(function (error) {
         // Handle Errors here.
         const errorCode = error.code;
         const errorMessage = error.message;
@@ -41,3 +46,46 @@ export default {
   },
 }
 </script>
+
+<style scoped lang="scss">
+.inner {
+  margin: 0 auto;
+  padding: 2rem;
+  width: 40rem;
+  border: .1rem solid #333;
+}
+.ttl {
+  text-align: center;
+  font-size: 1.8rem;
+  font-weight: 700;
+}
+.form {
+  margin-top: 3rem;
+}
+.form-list {
+  margin: 2rem 0rem;
+  display: flex;
+  align-items: center;
+}
+.form-label {
+  width: 30%;
+  font-size: 1.4rem;
+}
+.form-input {
+  padding: .5rem;
+  width: 70%;
+  border: .1rem solid #333;
+  font-size: 1.4rem;
+}
+.btn-wrap {
+  margin-top: 2rem;
+  text-align: center;
+}
+.btn {
+  padding: 1.2rem 2rem;
+  border-radius: 1.5rem;
+  background: #333;
+  color: #ffff;
+  font-size: 1.3rem;
+}
+</style>
--- src/pages/signUp/index.vue
+++ src/pages/signUp/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <section>
-    <h2>ユーザ登録画面</h2>
-    <form @submit.prevent="registerUser">
-      <div class="form-group">
-        <label for="email">メールアドレス:</label>
-        <input id="email" v-model="email" type="email" class="form-control">
+  <div class="inner">
+    <h1 class="ttl">ユーザ登録画面</h1>
+    <form class="form" @submit.prevent="registerUser">
+      <div class="form-list">
+        <label class="form-label" for="email">メールアドレス:</label>
+        <input id="email" v-model="email" type="email" class="form-input">
       </div>
-      <div class="form-group">
-        <label for="password">パスワード:</label>
-        <input id="password" v-model="password" type="password" class="form-control">
+      <div class="form-list">
+        <label class="form-label" for="password">パスワード:</label>
+        <input id="password" v-model="password" type="password" class="form-input">
       </div>
-      <button type="submit" class="btn btn-info">登録する</button>
+      <div class="btn-wrap">
+        <button type="submit" class="btn">登録する</button>
+      </div>
     </form>
-  </section>
+  </div>
 </template>
 
 <script lang="js">
@@ -20,16 +22,13 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export default {
   name: 'SignUpPage',
-  components: {
-  },
-  data () {
-    return {
-    }
-  },
   methods: {
     registerUser() {
       const auth = getAuth();
       createUserWithEmailAndPassword(auth, this.email, this.password)
+        .then(function () {
+          this.$router.push('/signIn');
+        })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
@@ -40,3 +39,53 @@ export default {
   },
 }
 </script>
+
+<style scoped lang="scss">
+.inner {
+  margin: 0 auto;
+  padding: 2rem;
+  width: 40rem;
+  border: .1rem solid #333;
+}
+
+.ttl {
+  text-align: center;
+  font-size: 1.8rem;
+  font-weight: 700;
+}
+
+.form {
+  margin-top: 3rem;
+}
+
+.form-list {
+  margin: 2rem 0rem;
+  display: flex;
+  align-items: center;
+}
+
+.form-label {
+  width: 30%;
+  font-size: 1.4rem;
+}
+
+.form-input {
+  padding: .5rem;
+  width: 70%;
+  border: .1rem solid #333;
+  font-size: 1.4rem;
+}
+
+.btn-wrap {
+  margin-top: 2rem;
+  text-align: center;
+}
+
+.btn {
+  padding: 1.2rem 2rem;
+  border-radius: 1.5rem;
+  background: #333;
+  color: #ffff;
+  font-size: 1.3rem;
+}
+</style>
