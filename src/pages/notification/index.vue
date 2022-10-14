6ad8dca8604888537e00a250ba49282514cc8960
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-10-14 21:44:34 +0900

    fix notification page.

--- src/pages/notification/index.vue
+++ src/pages/notification/index.vue
@@ -1,20 +1,37 @@
 <template>
-  <p>NotificationPage</p>
+  <div>
+    <h1 class="ttl">通知</h1>
+  </div>
 </template>
 
 <script lang="js">
-
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
+let auth;
 export default {
   name: 'NotificationPage',
   components: {
   },
   data () {
     return {
+      userInfo: [],
     }
   },
-  computed: {
-  },
-  methods: {
+  mounted() {
+    auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      this.userInfo = [];
+      if (user) {
+        this.userInfo = user
+      }
+    })
   }
 }
 </script>
+
+<style scoped lang="scss">
+.ttl {
+  margin-bottom: 3rem;
+  font-size: 2rem;
+  font-weight: 700;
+}
+</style>
