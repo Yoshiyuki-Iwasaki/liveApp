3ae5b7553c08246227d1b76d8daa53e0a704be93
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-10-14 21:44:44 +0900

    fix profile page.

--- src/pages/profile/index.vue
+++ src/pages/profile/index.vue
@@ -1,20 +1,40 @@
 <template>
-  <p>Profile</p>
+  <div>
+    <h1 class="ttl">プロフィール</h1>
+    <div class="txtArea">
+      <p class="email">email: {{userInfo.email}}</p>
+    </div>
+  </div>
 </template>
 
 <script lang="js">
-
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
+let auth;
 export default {
   name: 'ProfilePage',
   components: {
   },
   data () {
     return {
+      userInfo: [],
     }
   },
-  computed: {
-  },
-  methods: {
+  mounted() {
+    auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      this.userInfo = [];
+      if (user) {
+        this.userInfo = user
+      }
+    })
   }
 }
 </script>
+
+<style scoped lang="scss">
+.ttl {
+  margin-bottom: 3rem;
+  font-size: 2rem;
+  font-weight: 700;
+}
+</style>
