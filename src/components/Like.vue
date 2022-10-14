e2956be6ff5169f3b4c3792317fe341bb052538e
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-10-14 21:45:08 +0900

    fix like component.

--- src/components/Like.vue
+++ src/components/Like.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { collection, getDocs, addDoc, where, query, serverTimestamp } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from '@firebase/auth';
 import firebase from '@/firebase/firebase';
 let auth;
@@ -17,13 +17,27 @@ export default {
       userInfo: [],
     }
   },
-  mounted() {
+  async mounted() {
     auth = getAuth();
     onAuthStateChanged(auth, (user) => {
+      this.userInfo.uid = '';
       if (user) {
         this.userInfo = user
       }
     })
+    const q = query(collection(firebase, "likes"), where("video_id", "==", this.$router.history.current.params.id));
+    // const q = query(collection(firebase, "likes"), where("video_id", "==", this.$router.history.current.params.id), where("user_id", "==", this.userInfo.uid));
+    const querySnapshot = await getDocs(q);
+    const fbComments = [];
+    querySnapshot.forEach((doc) => {
+      const todo = {
+        user_id: doc.data().user_id,
+        video_id: doc.data().video_id,
+        createdAt: doc.data().createdAt,
+      }
+      fbComments.push(todo);
+    });
+    this.comments = fbComments;
   },
   methods: {
     async addLike() {
