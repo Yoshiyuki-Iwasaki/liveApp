1411f4354a9c41b64fe06d71da460d699c9b2634
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-10-28 01:54:38 +0900

    create profile page.

---
+++ src/pages/profile/_id.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1 class="ttl">プロフィール</h1>
+    <div class="txtArea">
+      <p class="email">email: {{ users[0] && users[0].email}}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+import { collection, getDocs, where, query } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
+import firebase from '@/firebase/firebase';
+let auth;
+export default {
+  name: 'ProfilePage',
+  components: {
+  },
+  data () {
+    return {
+      userInfo: [],
+      users: [],
+    }
+  },
+  async mounted() {
+    auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      this.userInfo = [];
+      if (user) {
+        this.userInfo = user
+      }
+    })
+
+    const usersQuery = query(collection(firebase, "users"), where("uid", "==", this.$router.history.current.params.id));
+    const usersQuerySnapshot = await getDocs(usersQuery);
+    const fbUsers = [];
+    usersQuerySnapshot.forEach((docs) => {
+      const user = {
+        id: docs.uid,
+        email: docs.data().email,
+        createdAt: docs.data().createdAt,
+      }
+      fbUsers.push(user);
+    });
+    this.users = fbUsers;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.ttl {
+  margin-bottom: 3rem;
+  font-size: 2rem;
+  font-weight: 700;
+}
+</style>
