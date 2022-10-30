2a5e4dd58b960fd26938b9086275fadd19c1c803
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-10-30 14:24:13 +0900

    create follow function.

--- src/pages/profile/_id.vue
+++ src/pages/profile/_id.vue
@@ -4,11 +4,13 @@
     <div class="txtArea">
       <p class="email">email: {{ users[0] && users[0].email}}</p>
     </div>
+    <button v-if="follow[0]" @click='removeFollow(follow[0])'>フォロー済み</button>
+    <button v-else @click='addFollow'>フォローボタン</button>
   </div>
 </template>
 
 <script lang="js">
-import { collection, getDocs, where, query } from "firebase/firestore";
+import { collection, doc, getDocs, addDoc, deleteDoc, where, query, serverTimestamp } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from '@firebase/auth';
 import { defineComponent } from 'vue';
 import firebase from '@/firebase/firebase';
@@ -21,6 +23,7 @@ export default defineComponent({
     return {
       userInfo: [],
       users: [],
+      follow: [],
     }
   },
   async mounted() {
@@ -29,6 +32,7 @@ export default defineComponent({
       this.userInfo = [];
       if (user) {
         this.userInfo = user
+        this.fetchFollow(this.userInfo.uid)
       }
     })
 
@@ -44,7 +48,31 @@ export default defineComponent({
       fbUsers.push(user);
     });
     this.users = fbUsers;
-  }
+  },
+  methods: {
+    async fetchFollow(userId) {
+      const followQuery = query(collection(firebase, "follow"), where("follow_id", "==", userId), where("follower_id", "==", this.$router.history.current.params.id));
+      const followQuerySnapshot = await getDocs(followQuery);
+      const fbFollow = [];
+      followQuerySnapshot.forEach((doc) => {
+        const follow = {
+          id: doc.id,
+        }
+        fbFollow.push(follow);
+      });
+      this.follow = fbFollow;
+    },
+    async addFollow() {
+      await addDoc(collection(firebase, "follow"), {
+        follow_id: this.userInfo.uid,
+        follower_id: this.$router.history.current.params.id,
+        createdAt: serverTimestamp()
+      });
+    },
+    async removeFollow(follow) {
+      await deleteDoc(doc(firebase, "follow", follow.id));
+    }
+  },
 })
 </script>
 
