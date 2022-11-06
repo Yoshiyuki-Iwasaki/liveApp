16c8cc9b8ef06c046627f0e9ef8142b3c36ed1a9
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-11-06 20:48:06 +0900

    create admin page.

---
+++ src/pages/admin/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1 class="ttl">管理画面</h1>
+  </div>
+</template>
+
+<script lang="js">
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
+import { defineComponent } from 'vue';
+let auth;
+export default defineComponent({
+  name: 'AdminPage',
+  components: {
+  },
+  data () {
+    return {
+      userInfo: [],
+    }
+  },
+  mounted() {
+    auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      this.userInfo = [];
+      if (user) {
+        this.userInfo = user
+      }
+    })
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.ttl {
+  margin-bottom: 3rem;
+  font-size: 2rem;
+  font-weight: 700;
+}
+</style>
