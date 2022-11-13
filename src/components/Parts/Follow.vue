38d45edfd033714127ca2d461df7a62f86bace5d
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-11-13 19:09:23 +0900

    create follow component.

--- src/components/Parts/Follow.vue
+++ src/components/Parts/Follow.vue
@@ -4,13 +4,51 @@
 </template>
 
 <script>
+import { collection, getDocs, where, query, doc, addDoc, deleteDoc, serverTimestamp } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
 import { defineComponent } from 'vue';
+import firebase from '@/firebase/firebase';
+let auth;
 export default defineComponent({
   name: 'Follow',
-  props: ['follow', 'addFollow', 'removeFollow'],
+  data() {
+    return {
+      follow: [],
+    }
+  },
+  mounted() {
+    auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      this.userInfo = [];
+      if (user) {
+        this.userInfo = user
+        this.fetchFollow(this.userInfo.uid)
+      }
+    })
+  },
+  methods: {
+    async fetchFollow(userId) {
+      const followQuery = query(collection(firebase, "follow"), where("follow_id", "==", userId), where("follower_id", "==", this.$router.history.current.params.id));
+      const followQuerySnapshot = await getDocs(followQuery);
+      const fbFollow = [];
+      followQuerySnapshot.forEach((doc) => {
+        const follow = {
+          id: doc.id,
+        }
+        fbFollow.push(follow);
+      });
+      this.follow = fbFollow;
+    },
+    async addFollow() {
+      await addDoc(collection(firebase, "follow"), {
+        follow_id: this.userInfo.uid,
+        follower_id: this.$router.history.current.params.id,
+        createdAt: serverTimestamp()
+      });
+    },
+    async removeFollow(follow) {
+      await deleteDoc(doc(firebase, "follow", follow.id));
+    }
+  },
 })
 </script>
-
-<style scoped lang="scss">
-
-</style>
--- src/pages/profile/_id.vue
+++ src/pages/profile/_id.vue
@@ -4,17 +4,15 @@
     <div class="txtArea">
       <p class="email">email: {{ users[0] && users[0].email}}</p>
     </div>
-    <Follow follow="follow" add-follow="addFollow" remove-follow="removeFollow"/>
+    <Follow />
   </div>
 </template>
 
 <script lang="js">
-import { collection, doc, getDocs, addDoc, deleteDoc, where, query, serverTimestamp } from "firebase/firestore";
-import { getAuth, onAuthStateChanged } from '@firebase/auth';
+import { collection, getDocs, where, query } from "firebase/firestore";
 import { defineComponent } from 'vue';
 import firebase from '@/firebase/firebase';
 import Follow from '@/components/Parts/Follow';
-let auth;
 export default defineComponent({
   name: 'ProfilePage',
   components: {
@@ -22,20 +20,10 @@ export default defineComponent({
   },
   data () {
     return {
-      userInfo: [],
       users: [],
-      follow: [],
     }
   },
   async mounted() {
-    auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      this.userInfo = [];
-      if (user) {
-        this.userInfo = user
-        this.fetchFollow(this.userInfo.uid)
-      }
-    })
 
     const usersQuery = query(collection(firebase, "users"), where("uid", "==", this.$router.history.current.params.id));
     const usersQuerySnapshot = await getDocs(usersQuery);
@@ -49,30 +37,6 @@ export default defineComponent({
       fbUsers.push(user);
     });
     this.users = fbUsers;
-  },
-  methods: {
-    async fetchFollow(userId) {
-      const followQuery = query(collection(firebase, "follow"), where("follow_id", "==", userId), where("follower_id", "==", this.$router.history.current.params.id));
-      const followQuerySnapshot = await getDocs(followQuery);
-      const fbFollow = [];
-      followQuerySnapshot.forEach((doc) => {
-        const follow = {
-          id: doc.id,
-        }
-        fbFollow.push(follow);
-      });
-      this.follow = fbFollow;
-    },
-    async addFollow() {
-      await addDoc(collection(firebase, "follow"), {
-        follow_id: this.userInfo.uid,
-        follower_id: this.$router.history.current.params.id,
-        createdAt: serverTimestamp()
-      });
-    },
-    async removeFollow(follow) {
-      await deleteDoc(doc(firebase, "follow", follow.id));
-    }
   },
 })
 </script>
